2bd5d95f5b41ea7c9f15230661147a598772d90e
Andre Santos <andreaugustodossantos@gmail.com>
2024-04-20 20:18:23 -0300

    v1

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+    <!-- Total... -->
+    <div class="p-4 bg-white rounded-lg shadow-md mt-4">
+      <h3 class="font-semibold text-gray-800">Total:</h3>
+      <p class="text-xl font-bold text-gray-900">R${{ total }}</p>
+    </div>
+    <!-- Purchase details... -->
+    <div v-for="product in products" :key="product.id">
+      <ProductCard
+        :item="product.item"
+        :description="product.description"
+        :value="product.value"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import ProductCard from "@/components/ProductCard.vue";
+import productData from "@/produtos.json";
+const products = ref(productData);
+
+const total = computed(() => {
+  return products.value.reduce((acc, product) => acc + product.value, 0);
+});
+</script>
