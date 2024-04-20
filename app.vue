a83adc216c33f8f3c39ebb5e17d448641ab927f0
Andre Santos <andreaugustodossantos@gmail.com>
2024-04-20 20:25:03 -0300

    test

--- app.vue
+++ app.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-    <!-- Total... -->
     <div class="p-4 bg-white rounded-lg shadow-md mt-4">
       <h3 class="font-semibold text-gray-800">Total:</h3>
-      <p class="text-xl font-bold text-gray-900">R${{ total }}</p>
+      <p class="text-xl font-bold text-gray-900">R${{ total.toFixed(2) }}</p>
     </div>
-    <!-- Purchase details... -->
-    <div v-for="product in products" :key="product.id">
+    <div>
       <ProductCard
+        v-for="product in products"
+        :key="product.id"
         :item="product.item"
         :description="product.description"
         :value="product.value"
+        @update-total="handleUpdateTotal"
       />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import ProductCard from "@/components/ProductCard.vue";
-import productData from "@/produtos.json";
-const products = ref(productData);
+import productData from "@/products.json";
 
-const total = computed(() => {
-  return products.value.reduce((acc, product) => acc + product.value, 0);
-});
+const products = ref(productData);
+const total = ref(0);
+
+// Event handler for updating the total
+const handleUpdateTotal = ({ value, selected }) => {
+  if (selected) {
+    total.value += value;
+  } else {
+    total.value -= value;
+  }
+};
 </script>
--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,21 +1,36 @@
 <template>
-  <div class="flex justify-between p-4 bg-white rounded-lg shadow-md mb-2 border-l-4 border-red-500">
+  <div
+    class="flex justify-between p-4 rounded-lg shadow-md mb-2 border-l-4 cursor-pointer"
+    :class="{
+      'bg-white border-red-500': !isSelected,
+      'bg-gray-300 border-gray-500': isSelected,
+    }"
+    @click="toggleSelection"
+  >
     <div>
       <h3 class="font-semibold text-gray-800">{{ item }}</h3>
       <p class="text-gray-600">{{ description }}</p>
     </div>
-    <div class="text-gray-800 font-semibold">
-      R${{ value.toFixed(2) }}
-    </div>
+    <div class="text-gray-800 font-semibold">R${{ value.toFixed(2) }}</div>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    item: String,
-    description: String,
-    value: Number,
-  },
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  item: String,
+  description: String,
+  value: Number,
+});
+
+const emit = defineEmits(["update-total"]);
+
+const isSelected = ref(false);
+
+const toggleSelection = () => {
+  isSelected.value = !isSelected.value;
+
+  emit("update-total", { value: props.value, selected: isSelected.value });
 };
 </script>
