f04d8f31db8335bfeedfb8098b0227ffea3a451b
Andre Santos <andreaugustodossantos@gmail.com>
2024-04-21 18:00:39 -0300

    botao voltar

--- components/CreditCardPayment.vue
+++ components/CreditCardPayment.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="">
-    <h3 class="text-lg font-bold mb-5">Preencha os dados de pagamento</h3>
+  <div>
+    <h3 v-if="!paymentSuccess" class="text-lg font-bold mb-5">
+      Preencha os dados de pagamento
+    </h3>
     <form v-if="!paymentSuccess" @submit.prevent="submitCreditCardForm">
       <div class="input-group mb-3">
         <label for="fullName" class="font-bold">Nome completo</label>
@@ -19,10 +21,15 @@
       <div class="flex flex-wrap -mx-2">
         <div class="input-group mb-3 px-2 w-40">
           <label for="expirationDate" class="font-bold">Validade</label>
-          <input type="text" placeholder="MM/AA" v-model="creditCard.expirationDate" required />
+          <input
+            type="text"
+            placeholder="MM/AA"
+            v-model="creditCard.expirationDate"
+            required
+          />
         </div>
         <div class="input-group mb-3 px-2 w-24">
-          <label for="cvv" class="font-bold ">CVV</label>
+          <label for="cvv" class="font-bold">CVV</label>
           <input type="text" placeholder="XXX" required />
         </div>
       </div>
@@ -37,10 +44,10 @@
         </button>
       </div>
     </form>
-    <div v-if="paymentSuccess" class="payment-success-message">
-      <p>Pagamento aprovado</p>
+    <div v-if="paymentSuccess" class="text-lg font-bold text-green-700 text-center mt-4">
+      <h1>Pagamento aprovado</h1>
     </div>
-    <div class="text-center justify-center mt-5">
+    <div class="text-center justify-center mt-5 text-center w-1/4 mx-auto text-white bg-button rounded-xl px-4 py-2 mt-7"> 
       <button @click="returnToPayment" class="return-button">Voltar</button>
     </div>
   </div>
